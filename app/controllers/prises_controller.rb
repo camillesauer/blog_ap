9f06e3e1998c151d96d7514e5c0960261eb3de48
Camille Sauer <camille-sauer@hotmail.fr>
2020-12-01 12:45:26 +0100

    ok

--- app/controllers/prises_controller.rb
+++ app/controllers/prises_controller.rb
@@ -39,7 +39,7 @@ class PrisesController < ApplicationController
 
   private
 
-  def article_params
+  def prise_params
     params.require(:prise).permit(:video_p, :img_p1, :img_p2, :img_p3, :img_p4, :img_p5, :legend_p1, :legend_p2, :legend_p3, :legend_p4, :legend_p5, :avatar_img_p1, :avatar_img_p2, :avatar_img_p3, :avatar_img_p4, :avatar_img_p5, :avatar_img_p6, :avatar_img_p7, :avatar_img_p8, :avatar_img_p9, :avatar_img_p10, :avatar_name_p1, :avatar_name_p2, :avatar_name_p3, :avatar_name_p4, :avatar_name_p5, :avatar_name_p6, :avatar_name_p7, :avatar_name_p8, :avatar_name_p9, :avatar_name_p10, :avatar_url_p1, :avatar_url_p2, :avatar_url_p3, :avatar_url_p4, :avatar_url_p5, :avatar_url_p6, :avatar_url_p7, :avatar_url_p8, :avatar_url_p9, :avatar_url_p10, :avatar_titre_p1, :avatar_titre_p2, :avatar_titre_p3, :avatar_titre_p4, :avatar_titre_p5, :avatar_titre_p6, :avatar_titre_p7, :avatar_titre_p8, :avatar_titre_p9, :avatar_titre_p10, :introduction_p1, :paraph_p1, :paraph_p2, :paraph_p3, :paraph_p4, :titre_p, :sous_titre_p, :date_p, :lieu_p, :lien_url1_p, :lien_url2_p)
   end
 end
